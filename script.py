6e9420a0cbdcbdacfa2e42ee8b22a0b37b1ee95a
Tobias Hermann <tobias.hermann@student.hs-rm.de>
2023-01-08 09:58:52 +0100

    modify get_author_name() to handle failed dict searches without try...except-statement

--- script.py
+++ script.py
@@ -22,15 +22,15 @@ def get_book(isbn):
     if response.status_code == 200:
         return response.json()
 
-    
+
 def get_author_name(author_key):
     """ Return name of author given a author_key
     """
-    
+
     name = ""
     url = "https://openlibrary.org{}.json".format(author_key)
     response = requests.get(url)
-    
+
     try:
         name = response.json().get("name")
     except KeyError:
@@ -43,7 +43,7 @@ def get_author_name(author_key):
 
 # open booklist file as "booklist"
 with open("booklist") as booklist:
-	books = booklist.read()
+    books = booklist.read()
 
 # remove linefeeds and whitespace
 books = books.replace("\n", "")
@@ -67,19 +67,19 @@ genres = [book.get("genres") for book in books_info]
 # remove NoneType elements
 genres = list(filter(None, genres))
 # flatten list to list of individual elements
-genres = sum(genres,[])
+genres = sum(genres, [])
 genres = np.array(genres)
 
 # get subjects
 subjects = [book.get("subjects") for book in books_info]
 subjects = list(filter(None, subjects))
-subjects = sum(subjects,[])
+subjects = sum(subjects, [])
 subjects = np.array(subjects)
 
 # get authors
 authors = [book.get("authors") for book in books_info]
 authors = list(filter(None, authors))
-authors = sum(authors,[])
+authors = sum(authors, [])
 authors = [get_author_name(author.get("key")) for author in authors]
 authors = np.array(authors)
 
@@ -88,10 +88,9 @@ print("\nYour Reading Wrapped")
 print("------------------------------")
 print("These are the books you have read:")
 for title in titles:
-	print("-", title)
-print("On average, you have read {0:0.2f} pages per book.".format(np.mean(pages)))
+    print("-", title)
+print("On average, you have read {0:0.2f} pages per book.".format(
+    np.mean(pages)))
 print("Your favorite author was {}.".format(st.mode(authors)))
 print("Your favorite genre was {}.".format(st.mode(genres)))
 print("Your favorite subject was {}.".format(st.mode(subjects)))
-
-
